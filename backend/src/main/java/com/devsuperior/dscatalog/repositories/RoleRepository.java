c52e0b0245e33fe4d8ee4b1f7ee4aae3ae78d50d
Allan <allanpereira773@gmail.com>
2026-06-17 22:18:01 -0300

    Repositories: CategoryRepository, ProductRepository, RoleRepository, UserRepository

---
+++ backend/src/main/java/com/devsuperior/dscatalog/repositories/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.devsuperior.dscatalog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<RoleRepository, Long> {
+}
