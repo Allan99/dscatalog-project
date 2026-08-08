45c5bd087b5f4e85ee9969f4a520704562eb5b98
Allan99 <allanpereira773@gmail.com>
2026-08-08 16:03:16 -0300

    Custom Validation Response

--- backend/src/main/java/com/devsuperior/dscatalog/controllers/exceptions/ControllerExceptionHandler.java
+++ backend/src/main/java/com/devsuperior/dscatalog/controllers/exceptions/ControllerExceptionHandler.java
@@ -5,6 +5,8 @@ import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -23,5 +25,22 @@ public class ControllerExceptionHandler {
         err.setMessage(e.getMessage());
         err.setPath(request.getRequestURI());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationError> methodArgumentNotFound(MethodArgumentNotValidException e,
+                                                        HttpServletRequest request){
+        ValidationError err = new ValidationError();
+        err.setTimestamp(Instant.now());
+        err.setStatus(HttpStatus.BAD_REQUEST.value());
+        err.setError("Validation error");
+        err.setMessage(e.getMessage());
+        err.setPath(request.getRequestURI());
+
+        for(FieldError f: e.getBindingResult().getFieldErrors()){
+            err.addError(f.getField(), f.getDefaultMessage());
+        }
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
     }
 }
